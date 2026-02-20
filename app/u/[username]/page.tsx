ad2da22e5bf4750c5260871029d0356d9a6a5cca
Blonde Glamazon <blondeglamazon@gmail.com>
2026-02-20 18:30:08 -0500

    profile update

--- app/u/[username]/page.tsx
+++ app/u/[username]/page.tsx
@@ -1,9 +1,6 @@
 import ProfileClient from './ProfileClient';
 
-export function generateStaticParams() {
-  // Giving it at least one dummy path forces the compiler to build the route!
-  return [{ username: 'user' }];
-}
+
 
 export default function Page({ params }: { params: { username: string } }) {
   return <ProfileClient username={params.username} />;
