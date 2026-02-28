fa070992897c5067462b16bae372d4b7b4abe730
Blonde Glamazon <blondeglamazon@gmail.com>
2026-02-27 19:56:06 -0500

    Hardcode public supabase keys for Appflow

--- app/lib/supabase/client.ts
+++ app/lib/supabase/client.ts
@@ -2,7 +2,7 @@ import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_URL ||  'https://bbecurcljmikzkflhapy.supabase.co',
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'Y=sb_secret_F-Zwz4kFVd8ps6zb07sJPw_lJPCqUAv'
   )
 }
