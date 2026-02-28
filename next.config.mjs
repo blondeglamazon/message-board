1294afdb1f2548ffedc77ca85bf431866a9c8e02
Blonde Glamazon <blondeglamazon@gmail.com>
2026-02-27 20:09:37 -0500

    Auto-detect Vercel vs Appflow build

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- //output: 'export',
-  images: { 
-    unoptimized: true 
-  },
-  skipProxyUrlNormalize: true,
+  // If we tell it we are building for Mobile, it exports statically. 
+  // Otherwise (like on Vercel), it leaves it alone!
+  output: process.env.VERCEL ? undefined : 'export',
+  images: {
+    unoptimized: true, // This is also required for Capacitor!
+  }
 };
 
 export default nextConfig;
