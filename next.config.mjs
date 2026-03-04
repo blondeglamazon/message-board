88791c1d8e5ea9f0ccf14475aeffd417329305d3
Blonde Glamazon <blondeglamazon@gmail.com>
2026-03-04 18:19:18 -0500

    prarams

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
   // MANUAL TOGGLE:
   // - Leave this as 'export' when building for mobile (Capacitor/Appflow)
   // - Comment this out or delete it when pushing to Vercel for web!
- output: 'export',
+ //output: 'export',
   
   images: {
     unoptimized: true, // Always leave this on for mobile static exports!
