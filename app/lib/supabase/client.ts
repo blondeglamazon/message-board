99b95f5076c34a1a6e93d70106884617dce5e2f6
Blonde Glamazon <blondeglamazon@gmail.com>
2026-03-06 11:21:05 -0500

    anon key updated in lib

--- app/lib/supabase/client.ts
+++ app/lib/supabase/client.ts
@@ -3,6 +3,6 @@ import { createBrowserClient } from '@supabase/ssr'
 export function createClient() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL ||  'https://bbecurcljmikzkflhapy.supabase.co',
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||'S_Pz_u30snnYnI610KDajw_ISKYlf-n'
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJiZWN1cmNsam1pa3prZmxoYXB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1NDMxMjYsImV4cCI6MjA4NTExOTEyNn0.27ghmb36D5Xpe0IoLtGNRlE51RWTIURenoltoMDWMfI'
   )
 }
