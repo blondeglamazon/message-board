dea4cd443a150be29c8b7dac2950b1e4cac74afb
Blonde Glamazon <blondeglamazon@gmail.com>
2026-03-06 19:53:27 -0500

    auth file update for google sign in

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,46 @@
+import { NextResponse } from 'next/server'
+import { createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+
+export async function GET(request: Request) {
+  const { searchParams, origin } = new URL(request.url)
+  const code = searchParams.get('code')
+  // if "next" is in param, use it as the redirect URL
+  const next = searchParams.get('next') ?? '/profile'
+  
+  if (code) {
+    const cookieStore = await cookies()
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          getAll() {
+            return cookieStore.getAll()
+          },
+          setAll(cookiesToSet) {
+            try {
+              cookiesToSet.forEach(({ name, value, options }) =>
+                cookieStore.set(name, value, options)
+              )
+            } catch (error) {
+              // The `setAll` method was called from a Server Component.
+              // This can be ignored if you have middleware refreshing user sessions.
+            }
+          },
+        },
+      }
+    )
+    
+    const { error } = await supabase.auth.exchangeCodeForSession(code)
+    
+    if (!error) {
+      return NextResponse.redirect(`${origin}${next}`)
+    } else {
+      console.error("Auth Callback Error:", error.message)
+    }
+  }
+
+  // If there's an error or no code, redirect to the login page with an error parameter
+  return NextResponse.redirect(`${origin}/login?error=CouldNotAuthenticate`)
+}
